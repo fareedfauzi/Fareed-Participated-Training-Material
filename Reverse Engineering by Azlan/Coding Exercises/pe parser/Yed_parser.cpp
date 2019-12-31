eb91df33f0c149686aacb7e9914a843e2dd9e33f
fareedfauzi <56353946+fareedfauzi@users.noreply.github.com>
2019-12-31 10:10:01 +0800

    Update Yed_parser.cpp

--- Reverse Engineering by Azlan/Coding Exercises/pe parser/Yed_parser.cpp
+++ Reverse Engineering by Azlan/Coding Exercises/pe parser/Yed_parser.cpp
@@ -1,14 +1,17 @@
-// Yed_parser.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Yed_parser.cpp : This file contains the 'main' function. Program execution begins and ends there
 
+
+
+//Nih antara library yang kita kena include, terutama sekali winnt.h. Sebab all struct PE ada dalam winnt.h. Windows.h tayah pun takpe bila dah ada winnt.h
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
 #include <winnt.h>
 
+//Untuk user pass argument ataupun parameter istilahnya. Contohnya Yed_parser.exe [nama file]. So, nama file tu adalah argument kita.
 int main(int argc, char** argv)
 {
-    //char c;
+    //Assign kan fp sebagai datatype FILE. So variable fp akan jadi our file.
     FILE* fp;
     unsigned long fileLen;
 
@@ -29,14 +32,15 @@ int main(int argc, char** argv)
     
     
     
-    // If argument not enough 
+    // If argument not enough. Contoh dia supply yed_parser without parameter 
     if (argc < 2) {
         printf("Usage : ./yed_parser.exe <filename>");
         exit(0);
     }
     
-    // Open file 
+    // Open file dalam readbyte(rb). Then iniate ke dalam fp.
     fp = fopen(argv[1], "rb");
+    //Kalau file tak jumpa, dia print mesej bawah tu.
     if (fp == NULL) {
         printf("\nCan't open file or file doesn't exist.");
         exit(0);
@@ -57,20 +61,22 @@ int main(int argc, char** argv)
     
 
     //Image_DOS_Header
-    dos_header = (PIMAGE_DOS_HEADER)mem; //get the first byte which is for DOS header
-    printf("============================\nIMAGE_DOS_HEADER\n============================\n");
+    dos_header = (PIMAGE_DOS_HEADER)mem; //get the first byte which is for DOS header. 0x00 byte.
+    printf("============================\nIMAGE_DOS_HEADER\n============================\n"); //refer line 16682 in winnt.h
+    //Nih member2 struct PIMAGE_DOS_HEADER yang important
     printf("e_magic: %x\n", dos_header->e_magic);
     printf("e_lfanew: %x\n", dos_header->e_lfanew);
 
 
     //Image_NT_Header
-    printf("\n============================\nIMAGE_NT_HEADERS\n============================\n");
+    printf("\n============================\nIMAGE_NT_HEADERS\n============================\n"); //refer line 16990 in winnt.h
     nt_header = (PIMAGE_NT_HEADERS)(mem + dos_header->e_lfanew); //to get the first byte for NT_HEADER. 1st byte of file (0x00) + value of e_lfanew (x value) = first byte of NT_HEADER
     printf("Signature: %x\n", nt_header->Signature);
 
     //Image_File_Header
     printf("\n============================\nIMAGE_FILE_HEADERS\n============================\n");
     file_header = (PIMAGE_FILE_HEADER)(mem + dos_header->e_lfanew + 0x4); // mem + dos_header->e_lfanew + 4 = nt_header
+    //Why 0x4? refer poster PE header yang abang Lan bagi.
     printf("Machine: %x\n", file_header->Machine);
     printf("NumberOfSections: %d\n", file_header->NumberOfSections);
     printf("TimeDateStamp: %x\n", file_header->TimeDateStamp);
@@ -82,6 +88,7 @@ int main(int argc, char** argv)
     //Image_Optional_Header
     printf("\n============================\nIMAGE_OPTIONAL_HEADER\n============================\n");
     optional_header = (PIMAGE_OPTIONAL_HEADER32)(mem + dos_header->e_lfanew + 0x18); // mem + dos_header->e_lfanew + 0x18 = optional_header32
+    ////Why 0x18? refer poster PE header yang abang Lan bagi.
     printf("Magic: %x\n", optional_header->Magic);
     printf("AddressOfEntryPoint: %x\n", optional_header->AddressOfEntryPoint);
     printf("ImageBase: %llx\n", optional_header->ImageBase);
@@ -100,7 +107,9 @@ int main(int argc, char** argv)
 
     int nsection = nt_header->FileHeader.NumberOfSections;
     for (int i = 0; i < nsection; i++) {
+        //Why 0xF8? refer poster PE header yang abang Lan bagi. Kira total Image optional header dari awal sampai habis akan dapat F8
         section_header = (PIMAGE_SECTION_HEADER)(mem + dos_header->e_lfanew + 0xF8 + next_section);
+        //next section akan tambah dengan 40bytes sbb size of each of section header 40 bytes.
         next_section = next_section + sizeof(IMAGE_SECTION_HEADER);
         printf("\n%s\n", section_header->Name);
         printf("RVA: %x\n", section_header->Misc.VirtualSize);
